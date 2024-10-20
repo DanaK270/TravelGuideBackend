ba600757741b265b6c3cc8f61644b6f8706d2ed5
Danak270 <daanaa227700@gmail.com>
2024-10-20 13:05:54 +0300

    done with delete and almost done with add

---
+++ controllers/review.js
@@ -0,0 +1,29 @@
+const { Review } = require('../models/Review')
+
+//create
+exports.reviews_add_post = (req, res) => {
+  // Save this data to a database probably
+  console.log('CREATE: ', req.body)
+  console.log('CREATE: ', res.locals.payload.id)
+
+  try {
+    let review = new Review({ ...req.body, user: res.locals.payload.id })
+    review.save()
+    res.send(review)
+  } catch (error) {
+    console.error('Error adding review:', error)
+    res.status(500).send('Error adding review')
+  }
+}
+
+//delete
+exports.reviews_delete = async (req, res) => {
+  const { id } = req.params
+  try {
+    await Review.findByIdAndDelete(id)
+    res.status(200).send('Review deleted successfully')
+  } catch (error) {
+    console.error(error)
+    res.status(500).send('Error deleting review')
+  }
+}
---
+++ routes/review.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const router = express.Router()
+const reviewsController = require('../controllers/review')
+const middleware = require('../middleware/index')
+
+//routes
+router.post(
+  '/add',
+  middleware.stripToken,
+  middleware.verifyToken,
+  reviewsController.reviews_add_post
+)
+
+//delete
+router.delete(
+  '/delete/:id',
+  middleware.stripToken,
+  middleware.verifyToken,
+  reviewsController.reviews_delete
+)
+
+module.exports = router
