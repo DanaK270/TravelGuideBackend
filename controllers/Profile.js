a3ee34de4cd9b896583b41dd0e22307d042b4a49
mohamed_7 <moodo1999712@gmail.com>
2024-10-22 14:56:56 +0300

    profile done

---
+++ controllers/Profile.js
@@ -0,0 +1,43 @@
+const { User } = require('../models/User')
+
+const GetUserById = async (req, res) => {
+  try {
+    let user = await User.findById(req.params.user_id)
+    if (!user) {
+      return res.status(404).send('User not found')
+    }
+    res.send({ user })
+  } catch (error) {
+    throw error
+  }
+}
+
+const UpdateUser = async (req, res) => {
+  try {
+    const existingUser = await User.findById(req.params.user_id)
+
+    if (!existingUser) {
+      return res.status(404).send('User not found')
+    }
+
+    let updatedData = req.body
+
+    if (req.file) {
+      updatedData.image = req.file.filename
+    }
+
+    const user = await User.findByIdAndUpdate(req.params.user_id, updatedData, {
+      new: true
+    })
+
+    res.send(user)
+  } catch (error) {
+    console.log('Error updating user:', error)
+    res.status(500).send('Error updating user')
+  }
+}
+
+module.exports = {
+  GetUserById,
+  UpdateUser
+}
---
+++ routes/Profile.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const router = express.Router()
+const multer = require('multer')
+const path = require('path')
+const upload = multer({ dest: path.join(__dirname, '../public/images/') })
+
+router.use(express.urlencoded({ extended: true }))
+
+//controller
+const ProfileCtrl = require('../controllers/Profile')
+
+//routes
+router.get('/:user_id', ProfileCtrl.GetUserById)
+router.put('/update/:user_id', ProfileCtrl.UpdateUser)
+
+module.exports = router
